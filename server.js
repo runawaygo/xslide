7db315a4b3a34c9f39d1b8eb185531ecc175e7fc
bagana <bagana@musiel.org>
2012-11-11 02:13:38 +0800

    More optimization on server.js. NOT TESTED\!

--- server.js
+++ server.js
@@ -1,5 +1,38 @@
 var DATA_FILE = "mm/data.json";
 
+var mindmapProvider = {
+  _instance: null,
+
+  get: function() {
+    if( !this._instance) this._instance = JSON.parse(fs.readFileSync(DATA_FILE));
+    return this._instance;
+  },
+
+  set: function(mindmap) {
+    // Detects vote pages and add voteCount properties and reset them.
+    mindmap.cachedVoteOptions = {};
+    for( var i = 0; i < mindmap.mindmap.root.children.length; ++i) {
+      var secondLevelNode = mindmap.mindmap.root.children[ i];
+      if( ":select" === secondLevelNode.text.caption.substring( secondLevelNode.text.caption.length - ":select".length))
+        for( var j = 0; j < secondLevelNode.children.length; ++j) {
+          var thirdLevelNode = secondLevelNode.children[ j];
+          thirdLevelNode.voteCount = 0;
+          mindmap.cachedVoteOptions[ thirdLevelNode.id] = thirdLevelNode;
+        }
+    }
+
+//    fs.writeFileSync(DATA_FILE, JSON.stringify(mindmap));
+
+    this._instance = mindmap;
+  },
+
+  getOption: function(id) {
+    return this._instance.cachedVoteOptions[ id];
+  },
+
+  Void: null
+}
+
 var fs = require('fs');
 var connect = require('connect');
 
@@ -21,58 +54,51 @@ var server = connect()
 
 .use('/', connect.static(__dirname + '/'))
 
+/**
+ * Handles the short URL for voting: Redirects to the original URL, with appropriate parameters
+ */
 .use('/c', function(req,res){
   res.statusCode = 307;
   res.setHeader('Location', '/client/app.html?key=' + req.url.substring(1));
   res.end();
 })
 
+/**
+ * When the presenter clicks on the Save button
+ */
 .use('/upload', function (req, res) {
   // req.body = {id:xxx , name: xxx, data: xxxx}
-  mindmap = JSON.parse(req.body.data);
-
-  // Detects vote pages and add voteCount properties and reset them.
-  mindmap.cachedVoteOptions = {};
-  for( var i = 0; i < mindmap.mindmap.root.children.length; ++i) {
-    var secondLevelNode = mindmap.mindmap.root.children[ i];
-    if( ":select" === secondLevelNode.text.caption.substring( secondLevelNode.text.caption.length - ":select".length))
-      for( var j = 0; j < secondLevelNode.children.length; ++j) {
-        var thirdLevelNode = secondLevelNode.children[ j];
-        thirdLevelNode.voteCount = 0;
-        mindmap.cachedVoteOptions[ thirdLevelNode.id] = thirdLevelNode;
-      }
-  }
-
-//  fs.writeFileSync(DATA_FILE, JSON.stringify(mindmap));
+  mindmapProvider.set( JSON.parse(req.body.data));
   return res.end();
 })
 
+/**
+ * Loaded by the 'Slide' page
+ */
 .use('/slide', function (req, res) {
-  if(!mindmap) mindmap = JSON.parse(fs.readFileSync(DATA_FILE));
-  res.end(JSON.stringify(mindmap));
+  res.end(JSON.stringify(mindmapProvider.get()));
 })
 
-.use('/slide/vote', function (req,res){
-  io.sockets.emit('vote', req.body.ids);
-  res.end();
-})
-
+/**
+ * Both for obtaining vote options and for actual voting
+ */
 .use('/client/vote', function (req,res){
-  if(req.method == 'GET') {
-    for( var i = 0; i < mindmap.mindmap.root.children.length; ++i) {
-      var secondLevelNode = mindmap.mindmap.root.children[ i];
-      if(secondLevelNode.id === req.query.id){
-        res.end(JSON.stringify(secondLevelNode));      
+  if(req.method == 'GET') { // Obtaining options. From the audience
+    var rootNode = mindmapProvider.get().mindmap.root;
+    for( var i = 0; i < rootNode.children.length; ++i) {
+      var secondLevelNode = rootNode.children[ i];
+      if(secondLevelNode.id === req.query.id) {
+        res.end(JSON.stringify(secondLevelNode));
         return;
       }
     }    
-  } else if(req.method == 'POST') {
+  } else if(req.method == 'POST') { // Submitting the vote. From the audience
   	var result = {};
     var ids = req.body.ids;
-    if(typeof ids == 'string') ids = [ids];
-  	for( var i = 0; i < ids.length; ++i) {
+    if(typeof ids == 'string') ids = [ids]; // The user checked only one option
+    for( var i = 0; i < ids.length; ++i) {
   	  var id = ids[ i] + '';
-      result[ id] =  ++mindmap.cachedVoteOptions[ id].voteCount;
+      result[ id] = ++mindmapProvider.getOption(id).voteCount;
   	}
     io.sockets.emit('vote', result);
     res.end('你已经提交成功！');
