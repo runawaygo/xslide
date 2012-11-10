5c6f7bd3a71924964d489527d450b0ac17c5cf77
bagana <bagana@musiel.org>
2012-11-11 01:51:06 +0800

    Clean up server.js a little bit

--- server.js
+++ server.js
@@ -1,29 +1,33 @@
-var connect, fs, io;
-fs = require('fs');
+var DATA_FILE = "mm/data.json";
 
-connect = require('connect');
+var fs = require('fs');
+var connect = require('connect');
 
+var io;
 var mindmap;
 
-var server = connect().use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    return next();
+var server = connect()
 
+.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  return next();
 })
+
 .use(connect.query())
+
 .use(connect.bodyParser())
+
 .use('/', connect.static(__dirname + '/'))
+
 .use('/c', function(req,res){
   res.statusCode = 307;
-  var id = req.url.substring(1);
-  res.setHeader('location', '/client/app.html?key='+ id);
+  res.setHeader('Location', '/client/app.html?key=' + req.url.substring(1));
   res.end();
 })
-.use('/upload', function (req, res) {
-  
 
+.use('/upload', function (req, res) {
   // req.body = {id:xxx , name: xxx, data: xxxx}
   mindmap = JSON.parse(req.body.data);
 
@@ -32,30 +36,29 @@ var server = connect().use(function (req, res, next) {
   for( var i = 0; i < mindmap.mindmap.root.children.length; ++i) {
     var secondLevelNode = mindmap.mindmap.root.children[ i];
     if( ":select" === secondLevelNode.text.caption.substring( secondLevelNode.text.caption.length - ":select".length))
-    	for( var j = 0; j < secondLevelNode.children.length; ++j) {
-    		var thirdLevelNode = secondLevelNode.children[ j];
-    		thirdLevelNode.voteCount = 0;
-    		mindmap.cachedVoteOptions[ thirdLevelNode.id] = thirdLevelNode;
-    	}
+      for( var j = 0; j < secondLevelNode.children.length; ++j) {
+        var thirdLevelNode = secondLevelNode.children[ j];
+        thirdLevelNode.voteCount = 0;
+        mindmap.cachedVoteOptions[ thirdLevelNode.id] = thirdLevelNode;
+      }
   }
-  
-  fs.writeFileSync('mm/' + 'data.json', JSON.stringify(mindmap));
+
+//  fs.writeFileSync(DATA_FILE, JSON.stringify(mindmap));
   return res.end();
 })
+
 .use('/slide', function (req, res) {
-  if(!mindmap) {
-    mindmap = JSON.parse(fs.readFileSync('mm/' + 'data.json'));
-  }
-  res.end(JSON.stringify(mindmap));    
+  if(!mindmap) mindmap = JSON.parse(fs.readFileSync(DATA_FILE));
+  res.end(JSON.stringify(mindmap));
 })
+
 .use('/slide/vote', function (req,res){
   io.sockets.emit('vote', req.body.ids);
   res.end();
 })
+
 .use('/client/vote', function (req,res){
-  if(req.method == 'GET')
-  {
-    console.log(mindmap)
+  if(req.method == 'GET') {
     for( var i = 0; i < mindmap.mindmap.root.children.length; ++i) {
       var secondLevelNode = mindmap.mindmap.root.children[ i];
       if(secondLevelNode.id === req.query.id){
@@ -63,30 +66,19 @@ var server = connect().use(function (req, res, next) {
         return;
       }
     }    
-  }
-  else if(req.method == 'POST')
-  {
+  } else if(req.method == 'POST') {
   	var result = {};
     var ids = req.body.ids;
     if(typeof ids == 'string') ids = [ids];
   	for( var i = 0; i < ids.length; ++i) {
-  		var id = ids[ i] + '';
-      console.log(id)
-      console.log(mindmap.cachedVoteOptions)
-      console.log(typeof id);
-  		result[ id] =  ++mindmap.cachedVoteOptions[ id].voteCount;
+  	  var id = ids[ i] + '';
+      result[ id] =  ++mindmap.cachedVoteOptions[ id].voteCount;
   	}
     io.sockets.emit('vote', result);
-
     res.end('你已经提交成功！');
   }
 })
+
 .listen(8000);
 
 io = require('socket.io').listen(server);
-io.sockets.on('connection', function (socket) {
-  // socket.emit('ping', { hello: 'world' });
-  // socket.on('pong', function (data) {
-  //   console.log(data);
-  // });
-});
